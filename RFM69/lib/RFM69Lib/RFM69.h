0df861c220d22120443270a74f7c3a3845eb237c
Bioresestance <aaron.bromma@gmail.com>
2020-06-26 15:49:27 -0700

    Defined outline of main class. Added register addr

---
+++ RFM69/lib/RFM69Lib/RFM69.h
@@ -0,0 +1,8 @@
+
+
+class RFM69 {
+    public:   
+        // Enumeration of all of the possible registers in the RFM69HCW
+        enum class registerAddresses;
+
+};
--- RFM69/src/main.cpp
+++ RFM69/src/main.cpp
@@ -1,4 +1,11 @@
 #include <Arduino.h>
+#include <..\lib\RFM69Lib\RFM69.h>
+#include <esp_freertos_hooks.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_task_wdt.h"
+
+static RFM69 module;
 
 void setup() {
   // put your setup code here, to run once:
@@ -6,4 +13,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 }
