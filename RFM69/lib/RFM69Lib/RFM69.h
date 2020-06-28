223d9c4ef1cd9ed5793967230e261374b13381bf
bioresestance <aaron.bromma@gmail.com>
2020-06-28 16:21:51 -0700

    Added new register classes

--- RFM69/lib/RFM69Lib/RFM69.h
+++ RFM69/lib/RFM69Lib/RFM69.h
@@ -1,4 +1,18 @@
+#pragma once
 #include "stdint.h"
+
+/**
+ * @brief Function point for SPI read function.
+ * 
+ * @param first address of SPI chip, gotten from init function.
+ * @param second Number of bytes to read. 
+ * @param third Buffer to store results of read. Ensure at least length long.
+ * 
+ * @return Whether the result was successful or not.
+ * 
+ */
+typedef bool (*RFM69spiReadFnc)(uint8_t , uint16_t, uint8_t*);
+
 
 class RFM69 {
     public:  
@@ -7,11 +21,8 @@ class RFM69 {
         // Defaults
         RFM69();
         ~RFM69(); 
-
-
-
-        /***************Register Related Definitions************************************/
-
+        /*********************************************************************************/
+        
         
 
 
@@ -24,16 +35,22 @@ enum class RFM69RegisterAddresses;
 
 class RFM69Register {
 protected:
-    const uint8_t _default_value;
-    const uint8_t _recommended_value;
-    const uint8_t _address;
+    uint8_t _default_value;
+    uint8_t _recommended_value;
+    RFM69RegisterAddresses _address;
 
 public:
     uint8_t get_default_value()  {return _default_value;}
     uint8_t get_recommended_value() { return _recommended_value;}
-    uint8_t get_address() {return _address;}
+    uint8_t get_address() {return (uint8_t)_address;}
  
     //! Returns the Current set value.
-    virtual uint8_t getValue() = 0;
+    virtual uint8_t get_value() = 0;
+
+    RFM69Register(uint8_t default_value, uint8_t recommended_value, RFM69RegisterAddresses address) {
+        _default_value = default_value;  
+        _recommended_value = recommended_value;
+        _address = address;
+    }
 
 };
--- RFM69/lib/RFM69Lib/RFM69Registers.cpp
+++ RFM69/lib/RFM69Lib/RFM69Registers.cpp
@@ -1,5 +1,25 @@
 #include "RFM69.h"
 
+/* a=target variable, b=bit number to act upon 0-n */
+#define BIT_SET(a,b) ((a) |= (1ULL<<(b)))
+#define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
+#define BIT_FLIP(a,b) ((a) ^= (1ULL<<(b)))
+#define BIT_CHECK(a,b) (!!((a) & (1ULL<<(b))))        // '!!' to make sure this returns 0 or 1
+
+/* x=target variable, y=mask */
+#define BITMASK_SET(x,y) ((x) |= (y))
+#define BITMASK_CLEAR(x,y) ((x) &= (~(y)))
+#define BITMASK_FLIP(x,y) ((x) ^= (y))
+#define BITMASK_CHECK_ALL(x,y) (((x) & (y)) == (y))   // warning: evaluates y twice
+#define BITMASK_CHECK_ANY(x,y) ((x) & (y))
+
+
+//! a = target, b = bit to set, c = condition to check. 
+
+//Only sets bit if C == true.
+#define BIT_SET_IF_TRUE(a,b,c) ( (c) ? BIT_SET(a,b) :  )
+// Only sets bit if C == false.
+#define BIT_SET_IF_FALSE(a,b,c) ( (c) ?  :  BIT_SET(a,b) )
 
 /**
  *  @brief Address values for all Registers that can be accessed.
@@ -92,4 +112,29 @@ enum class RFM69RegisterAddresses
             RegTestPa2 = 0x5C,
             RegTestDagc = 0x6F,
             RegTestAfc = 0x71,
+};
+
+struct RegOpMode : public RFM69Register 
+{
+    bool _sequencerOff;
+    bool _listenOn;
+    bool _listenAbort;
+    uint8_t _mode; // Only uses 3 bits.
+
+    RegOpMode(bool sequencerOff, bool listenOn, bool listenAbort, uint8_t mode) 
+    : RFM69Register(0x04, 0x04, RFM69RegisterAddresses::RegOpMode) {
+        _sequencerOff = sequencerOff;
+        _listenOn = listenOn;
+        _listenAbort = listenAbort;
+        _mode = mode;
+    }
+
+    uint8_t get_value() {
+        uint8_t byte = 0;
+
+        byte |= (_mode & 0x07) << 1;
+        //byte |= BIT
+
+        return byte;
+    }
 };
