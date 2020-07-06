da25d9b14a06e24656e78d565f8551cfbd373cd8
Bioresestance <aaron.bromma@gmail.com>
2020-07-06 14:21:06 -0700

    Removed old macros.

--- RFM69/lib/RFM69Lib/RFM69Registers.cpp
+++ RFM69/lib/RFM69Lib/RFM69Registers.cpp
@@ -1,26 +1,5 @@
 #include "RFM69.h"
 
-/* a=target variable, b=bit number to act upon 0-n */
-#define BIT_SET(a,b) ((a) |= (1ULL<<(b)))
-#define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
-#define BIT_FLIP(a,b) ((a) ^= (1ULL<<(b)))
-#define BIT_CHECK(a,b) (!!((a) & (1ULL<<(b))))        // '!!' to make sure this returns 0 or 1
-
-/* x=target variable, y=mask */
-#define BITMASK_SET(x,y) ((x) |= (y))
-#define BITMASK_CLEAR(x,y) ((x) &= (~(y)))
-#define BITMASK_FLIP(x,y) ((x) ^= (y))
-#define BITMASK_CHECK_ALL(x,y) (((x) & (y)) == (y))   // warning: evaluates y twice
-#define BITMASK_CHECK_ANY(x,y) ((x) & (y))
-
-
-//! a = target, b = bit to set, c = condition to check. 
-
-//Only sets bit if C == true.
-#define BIT_SET_IF_TRUE(a,b,c) ( (c) ? BIT_SET(a,b) :  )
-// Only sets bit if C == false.
-#define BIT_SET_IF_FALSE(a,b,c) ( (c) ?  :  BIT_SET(a,b) )
-
 /**
  *  @brief Address values for all Registers that can be accessed.
  *  @note The order that the values are defined are critical. Do not change unless the datasheet is consulted.
